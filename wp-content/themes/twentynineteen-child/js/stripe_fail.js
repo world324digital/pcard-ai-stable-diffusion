593c4cd75eada5e95c3d980d2a34969dad69f173
world324digital <world.digital.dev@gmail.com>
2022-11-14 16:39:46 +0300

    adding stripe part js

---
+++ wp-content/themes/twentynineteen-child/js/stripe_fail.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    localStorage.removeItem('credit_product_id');
+}); 
---
+++ wp-content/themes/twentynineteen-child/js/stripe_success.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var credit_product_id = localStorage.getItem('credit_product_id');
+    if (credit_product_id != undefined) {
+        addCredits();
+    }
+
+    function addCredits() {
+        var data = {
+            'action': 'add_credits_action',
+            'credit_product_id': credit_product_id,
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: CONFIG.ajaxUrl,
+            data: data,
+            success: function (response) {
+                var result = JSON.parse(response);
+                localStorage.removeItem('credit_product_id');
+                console.log(result);
+            }
+        });
+    }
+}); 
