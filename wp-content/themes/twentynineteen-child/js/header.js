9db1fe02aef33ea0ed1554178f6abed13d36d830
world324digital <world.digital.dev@gmail.com>
2022-11-14 16:41:14 +0300

    adding credit part in header

---
+++ wp-content/themes/twentynineteen-child/js/header.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    getUserCredits();
+
+    $('.create-header a').click(function (e) {
+        e.preventDefault();
+        var prompt = $('#prompt').val();
+        prompt = "Landscape with a Light Blue Haze on a Clear Mirror Lake";
+
+
+        if (prompt != '') {
+            var data = {
+                'action': 'predict_action',
+                'prompt': prompt,
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: CONFIG.ajaxUrl,
+                data: data,
+                success: function (response) {
+                    var result = JSON.parse(response);
+                    
+                    // Generatign 6 images
+                    // if (result.klms != undefined && result.ddim != undefined) {
+                    //     localStorage.setItem('prompt', prompt);
+                    //     window.location.href = CONFIG.siteUrl + '/create-image?id1=' + result.klms + '&id2=' + result.ddim;
+                    // }
+                    
+                    // Generating 3 images 
+                    if (result.klms != undefined) {
+                        localStorage.setItem('prompt', prompt);
+                        window.location.href = CONFIG.siteUrl + '/create-image?id1=' + result.klms;
+                    } else if (result.status == 'failed') {
+                        alert(result.message);
+                    }
+                }
+            });
+
+        } else {
+            return false;
+        }
+    });
+
+    $('.credit-menu').click(function () {
+        $('#wow-modal-id-2').trigger('click');
+    });
+
+    $('.pricing-item .item-right input[type="submit"]').click(function (e) {
+        e.preventDefault();
+        var product_id = $(this).parent().find('input[name="wpsc_product_id"]').val();
+        var parent_form = $(this).parent();
+        localStorage.removeItem('credit_product_id');
+        localStorage.setItem('credit_product_id', product_id);
+        parent_form.submit();
+    });
+
+    function getUserCredits() {
+        $.ajax({
+            type: 'POST',
+            url: CONFIG.ajaxUrl,
+            data: {
+                'action': 'get_user_credits_action'
+            },
+            success: function (response) {
+                var result = JSON.parse(response);
+                $('#credit-value').text(result);
+                $('.credit_value').text(result);
+
+            }
+        });
+    }
+});
